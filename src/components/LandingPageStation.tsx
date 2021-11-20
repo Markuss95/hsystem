63e550d0342f833eb1ed2a03ea1dc691678182fc
unknown <dreykey94@gmail.com>
2021-11-20 09:19:40 +0100

    Bcg for landingpagestation

--- src/components/LandingPageStation.tsx
+++ src/components/LandingPageStation.tsx
@@ -1,13 +1,30 @@
 import styled from "styled-components";
+import hydrogenStation from "../assets/HydrogenStation.jpg";
 const LandingPageStation = () => {
-  return <Wrapper></Wrapper>;
+  return (
+    <Wrapper>
+      <img
+        className="background-img"
+        src={hydrogenStation}
+        alt="Hydrogen Station"
+      />
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.div`
   position: relative;
   height: 100vh;
   width: 100vw;
-  background: gray;
+
+  .background-img {
+    position: absolute;
+    object-fit: cover;
+    top: 0;
+    left: 0;
+    height: 100vh;
+    width: 100vw;
+  }
 `;
 
 export default LandingPageStation;
