d0b8159b80df4727a7f125161d07c4620733d17e
unknown <dreykey94@gmail.com>
2021-11-20 09:05:38 +0100

    Added number 2 to H symbol

--- src/components/LandingPageHydrogen.tsx
+++ src/components/LandingPageHydrogen.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
-
 import hydrogen from "../assets/shutterstock_10926873743.png";
+import two from "../assets/shutterstock_1092687374.4.png";
 
 const LandingPageHydrogen = ({ scrollPos }: { scrollPos: boolean }) => {
   return (
@@ -10,6 +10,11 @@ const LandingPageHydrogen = ({ scrollPos }: { scrollPos: boolean }) => {
           className={`hydrogen-img ${scrollPos ? "hidden" : ""}`}
           src={hydrogen}
           alt="Hydrogen"
+        />
+        <img
+          className={`two-img ${scrollPos ? "hidden" : ""}`}
+          src={two}
+          alt="Two"
         />
       </div>
     </Wrapper>
@@ -38,9 +43,33 @@ const Wrapper = styled.div`
     position: fixed;
     left: 50%;
     top: 51%;
-    transform: translate(-51%, -50%);
+    transform: translate(-50%, -51%);
     height: 8rem;
     width: 8rem;
+  }
+  .two-img {
+    object-fit: cover;
+    position: fixed;
+    margin-left: 4.5rem;
+    left: 50%;
+    top: 57%;
+    transform: translate(-50%, -57%);
+    height: 12rem;
+    width: 12rem;
+  }
+  @media screen and (min-width: 900px) {
+    .hydrogen-img {
+      height: 13rem;
+      width: 13rem;
+    }
+    .two-img {
+      height: 20rem;
+      width: 20rem;
+      margin-left: 8rem;
+      left: 50%;
+      top: 61%;
+      transform: translate(-50%, -61%);
+    }
   }
 `;
 export default LandingPageHydrogen;
